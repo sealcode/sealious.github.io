d51f249952b8379fb586c73c1470d95f11f32d36
debian-sh <bestmatrix@gmail.com>
2015-10-05 18:12:27 +0200

    Fix in responsive-navbar

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -1,12 +1,13 @@
-function show(){
-    if(document.querySelector('#navigation').style.height == '17.45rem') {
-        document.querySelector('#navigation').style.height= '4rem';
-    }
-    else
-    {
-        document.querySelector('#navigation').style.height= '17.45rem';
-    }
+function show() {
+	// if (document.querySelector('#navigation').style.height ===='17.45rem') {
+	if (document.querySelector('#navigation').style.height ===='14rem') {
+		document.querySelector('#navigation').style.height = '4rem';
+	} else {
+		// document.querySelector('#navigation').style.height = '17.45rem';
+		document.querySelector('#navigation').style.height = '14rem';
+	}
 }
-function hide(){
-    document.querySelector('#navigation').style.height= '4rem';
+
+function hide() {
+	document.querySelector('#navigation').style.height = '4rem';
 }
